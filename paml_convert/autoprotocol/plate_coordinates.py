347788ece9daacc2c3ea180c56159e97113638e1
Daniel Bryce <danbryce410@gmail.com>
2021-11-11 17:04:09 -0600

    Spec for PrimitiveArray

--- paml_convert/autoprotocol/plate_coordinates.py
+++ paml_convert/autoprotocol/plate_coordinates.py
@@ -2,6 +2,26 @@ from autoprotocol.container import Container, WellGroup
 from string import ascii_letters
 import re
 
+def num2col(num: int):
+    """
+    Get the alpha column string from the index.
+    - 1 -> A
+    - 26 -> Z
+    - 27 -> AA
+    - 52 -> AZ
+    - etc
+    """
+    if num < 1:
+        raise ValueError("Cannot convert num to column, num is too small.")
+    col = ""
+    while True:
+        if num > 26:
+            num, r = divmod(num - 1, 26)
+            col = chr(r + ord('A')) + col
+        else:
+            return chr(num + ord('A') - 1) + col
+
+
 def col2num(col: str):
     """
     Get the index of the alpha column string.
@@ -32,11 +52,11 @@ def coordinate_rect_to_row_col_pairs(coords: str):
         raise Exception(f"Invalid coordinates: {coords}")
     fcol, frow = coordinate_to_row_col(parts[0])
     scol, srow = coordinate_to_row_col(parts[1])
-    
+
     indices = []
     for i in range(fcol, scol + 1):
         for j in range(frow, srow + 1):
-            indices.append((i, j))
+            indices.append((j, i))
     return indices
 
 def coordinate_rect_to_well_group(container: Container, coordinates: str):
